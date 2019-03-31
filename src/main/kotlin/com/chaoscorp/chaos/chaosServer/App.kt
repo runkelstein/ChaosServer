ba3af2a00ef08e973a9fb09e8568e20f0953df03
martin domnick <martin_domnick@web.de>
2019-03-31 13:13:10 +0200

    Start The Dam Project

---
+++ src/main/kotlin/com/chaoscorp/chaos/chaosServer/App.kt
@@ -0,0 +1,15 @@
+package com.chaoscorp.chaos.chaosServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App {
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<App>(*args)
+		}
+	}
+
+}
---
+++ src/main/kotlin/com/chaoscorp/chaos/chaosServer/config/AppConfig.kt
@@ -0,0 +1,11 @@
+package com.chaoscorp.chaos.chaosServer.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("com.chaoscorp.chaos.chaosServer")
+class AppConfig {
+
+
+}
